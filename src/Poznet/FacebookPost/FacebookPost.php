f053a824481c0a51c1aa78b4daa64c146784b527
Michał Glajc <j00s3ph@gmail.com>
2015-06-02 12:16:45 +0200

    basics

--- src/Poznet/FacebookPost/FacebookPost.php
+++ src/Poznet/FacebookPost/FacebookPost.php
@@ -1,17 +1,36 @@
 <?php
 
 namespace Poznet\FacebookPost;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of FacebookPost
- *
- * @author michalg
- */
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphObject;
+use Facebook\FacebookRequestException;
+
+ 
 class FacebookPost {
-    //put your code here
+    
+    private $config = array();
+    
+    public function __construct($id=NULL,$secret=NULL,$token) {
+        $this->config['appId'] = $id;
+        $this->config['secret'] = $secret;
+        $this->config['fileUpload'] =false;
+        
+        $fb = new Facebook($config);
+ 
+// define your POST parameters (replace with your own values)
+$params = array(
+  "access_token" => "YOUR_ACCESS_TOKEN", // see: https://developers.facebook.com/docs/facebook-login/access-tokens/
+  "message" => "Here is a blog post about auto posting on Facebook using PHP #php #facebook",
+  "link" => "http://www.pontikis.net/blog/auto_post_on_facebook_with_php",
+  "picture" => "http://i.imgur.com/lHkOsiH.png",
+  "name" => "How to Auto Post on Facebook with PHP",
+  "caption" => "www.pontikis.net",
+  "description" => "Automatically post on Facebook with PHP using Facebook PHP SDK. How to create a Facebook app. Obtain and extend Facebook access tokens. Cron automation."
+);
+        
+        //
+     
+}
 }
