46989783a1f3ce93c108c0d2289cfacaf9772f20
Michał Glajc <j00s3ph@gmail.com>
2015-06-02 11:33:58 +0200

    basics

---
+++ src/Poznet/FacebookPost/FacebookPost.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Poznet\FacebookPost;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of FacebookPost
+ *
+ * @author michalg
+ */
+class FacebookPost {
+    //put your code here
+}
